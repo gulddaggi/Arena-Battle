a7cefc1f308c08084e71af73ec05acc46b2887b6
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-01-25 17:23:48 +0900

    Migration pawn to character & Character view
    - 플레이어 폰 클래스를 캐릭터 클래스로 변경
    - 3인칭 캐릭터 시점 구현(GTA & DIABLO)

--- ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
+++ ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeArenaBattle_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x0D220273,
-				0x7321EEB0,
+				0xD90EFA81,
+				0x8BF238F9,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
